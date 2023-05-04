46d7088e2aa405cdb1a0b7a0d734ef9cde0b8f88
bgibso30 <128432574+bgibso30@users.noreply.github.com>
2023-05-04 11:36:55 -0400

    Update ufo_app.py

--- ufo_app.py
+++ ufo_app.py
@@ -5,6 +5,7 @@ from wordcloud import WordCloud, STOPWORDS
 import glob, nltk, os, re
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
+from PIL import Image
 from ufo_app2 import *
 
 
@@ -37,47 +38,80 @@ ufo_df = ufo_df.astype({'Summary':'string'})
 
 #Sidebar
 st.sidebar.header('Bar Chart Settings')
-state_count = st.sidebar.slider('Top number of states to display', min_value=5, max_value=25, value=10, step=2)
+state_count = st.sidebar.slider('Number of bars to display', min_value=5, max_value=25, value=10, step=2)
 
 st.sidebar.header('Word Cloud Settings')
 max_word = st.sidebar.slider('Max Words', min_value=10, max_value=200, value=100, step=10)
 word_size = st.sidebar.slider('Size of largest word', min_value=50, max_value=350, value=60, step=10)
 
-row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
+#Create vertical space
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+image = Image.open('ufo.jpg')
+st.sidebar.image(image, use_column_width=True)
+
+
+row0_spacer1, row0_1, row0_spacer2, row0_spacer3 = st.columns((.1, 2.3, 1.3, .1))
 with row0_1:
     st.title('Visualizing UFO Sightings')
-with row0_2:
-    st.text("")
-    st.subheader('Streamlit App by [Bria Gibson](https://www.linkedin.com/in/bria-gibson-ba0573188/)')
 row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 with row3_1:
-    st.markdown("Have you ever wondered how often a UFO is sighted across the country? Check out these interesting statistics from the year 2016!")
+    st.markdown("Have you ever wondered how often a UFO is sighted across the country? Check out these interesting statistics from the year 2016!  This is a Streamlit App by [Bria Gibson](https://www.linkedin.com/in/bria-gibson-ba0573188/) ")
 
 #Create vertical space
 st.write(" ")
 st.write(" ")
 st.write(" ")
-st.subheader('Count of UFO Sightings by State') 
+st.subheader('Count of UFO Sightings') 
+
+
+
+state_list= ufo_df[ufo_df['Country'] == 'USA']
+state_list = state_list['State'].unique().tolist()
+state_list.sort(reverse=False)
+state_list.insert(0, "Top States")
+
+state = st.selectbox('Select a State to look at city count', state_list, index=0)
+
 
 
 
 #Graph
 
-bar_chart = alt.Chart(ufo_df).transform_aggregate(
-    Count='count()',
-    groupby=['State']
-).transform_window(
-    rank='rank(count)',
-    sort=[alt.SortField('Count', order='descending')]
-).transform_filter(
-    alt.datum.rank < state_count + 1
-).mark_bar().encode(
-    y=alt.Y('State:N', sort='-x'),
-    x=('Count:Q')
-).properties(
-    width=700,
-    height=450
-)
+if state == "Top States":
+    bar_chart = alt.Chart(ufo_df).transform_aggregate(
+        Count='count()',
+        groupby=['State']
+    ).transform_window(
+        rank='rank(count)',
+        sort=[alt.SortField('Count', order='descending')]
+    ).transform_filter(
+        alt.datum.rank < state_count + 1
+    ).mark_bar().encode(
+        y=alt.Y('State:N', sort='-x'),
+        x=('Count:Q')
+    ).properties(
+        width=700,
+        height=450
+    )
+else:
+      cities_df = ufo_df[(ufo_df['Country'] == 'USA') & (ufo_df['State'] == state)]
+      bar_chart = alt.Chart(cities_df).transform_aggregate(
+        Count='count()',
+        groupby=['City']
+    ).transform_window(
+        rank='rank(count)',
+        sort=[alt.SortField('Count', order='descending')]
+    ).transform_filter(
+        alt.datum.rank < state_count + 1
+    ).mark_bar().encode(
+        y=alt.Y('City:N', sort='-x'),
+        x=('Count:Q')
+    ).properties(
+        width=700,
+        height=450
+    )
+
 st.altair_chart(bar_chart)
 
 #Create vertical space
